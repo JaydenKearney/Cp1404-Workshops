d038b524e23ecf4b61afe0ace33d50cd7b7ecb1d
Jayden Kearney <jayden.kearney@my.jcu.edu.au>
2018-03-26 17:09:44 +1000

    Modify user-input, clear-text button, and add Label

--- Pratical 6 - Kivy/box_layout_demo.py
+++ Pratical 6 - Kivy/box_layout_demo.py
@@ -8,5 +8,11 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+    def clear_text(self):
+        self.root.ids.input_name.text = ''
+        self.root.ids.output_label.text = ''
 
 BoxLayoutDemo().run()
